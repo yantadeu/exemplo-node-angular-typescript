193e8857a115a0c8667a3d6d18d934c69399afba
yantadeu <yan.tadeu1@gmail.com>
2018-01-05 17:07:06 -0200

    ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import {HelltriangleComponent} from './helltriangle/helltriangle.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelltriangleComponent
   ],
   imports: [
     BrowserModule,
